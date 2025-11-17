dd4f16182a5c00f1c622062d6f43e03390223616
alexm <alex.manea12@gmail.com>
2025-11-17 14:32:35 +0200

    Execute first request

--- extraction/data_extraction.py
+++ extraction/data_extraction.py
@@ -1,6 +1,17 @@
 import os
+from googleapiclient.discovery import build
+
 
 # get youtube api key from environment variable (for windows)
 api_key = os.environ.get("YOUTUBE_API_KEY")
 
-print(f"API Key: {api_key}")
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.channels().list(
+    part='statistics',
+    forUsername='GoogleDevelopers',
+)
+
+response = request.execute()
+
+print(response)
